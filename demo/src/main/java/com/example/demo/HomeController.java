a417c5c2b09c7dc5e75d171321d48d9681780835
hariharan8428978917 <hariharansekar11@gmail.com>
2024-09-18 19:40:31 +0530

    Home Controller

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("hello")
 public class HomeController {
 	
-	
-	
+
+	public HomeController() {
+		super();
+		System.out.println("HOme controller");
+	}
+
 	@GetMapping("hi")
 	public String get() {
 		return "hi";
